a124e2e38a8ab14ddbbffaae130dd606949e0cc3
b15174 <55978263+b15174@users.noreply.github.com>
2024-07-17 02:11:07 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://cn.tradingview.com/";//your website url
+  let target = "https://cn.tradingview.com/chart/?symbol=SSE%3A601398";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
